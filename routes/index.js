c635862039a81b51f33664e4ac57804e9025e89e
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-09-20 15:12:22 +0100

    iteration 2, added route guard

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const router = Router();
+const bcryptjs = require('bcryptjs');
+const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
   res.render('index');
@@ -7,6 +9,36 @@ router.get('/', (req, res, next) => {
 
 router.get('/register', (req, res, next) => {
   res.render('register');
+});
+
+router.get('/log-in', (req, res, next) => {
+  res.render('log-in');
+});
+
+router.post('/log-in', (req, res, next) => {
+  const { username, password } = req.body;
+  let user;
+  User.findOne({ username })
+    .then((document) => {
+      user = document;
+      if (!user) {
+        throw new Error('ACCOUNT_NOT_FOUND');
+      } else {
+        return bcryptjs.compare(password, user.passwordHashAndSalt);
+      }
+    })
+    .then((comparisonResult) => {
+      if (comparisonResult) {
+        console.log('User was authenticated');
+        req.session.userId = user._id;
+        res.redirect('/');
+      } else {
+        throw new Error('WRONG_PASSWORD');
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
